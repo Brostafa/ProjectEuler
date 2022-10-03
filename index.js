d729f7605fe47a9721dd0c971d89122fe49861e0
Mostafa Hefny <mostafahefny@Mostafa-MBP.local>
2022-10-03 19:01:37 +0200

    solved

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const isPrime = num => {
   if (num === 2) return true
   if (num % 2 === 0) return false
 
-  // prime are only odd
+  // primes are only odd numbers
   for (let i = 3; i <= Math.sqrt(num); i += 2) {
     if (num % i === 0) return false 
   }
