b4ede122b7d319214fd7f64b5cf3fdae4144fb7b
Mostafa Hefny <mostafahefny@Mostafa-MBP.local>
2022-10-03 18:59:11 +0200

    solved

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
 /**
  * @problem_num
- * 
+ * Problem 10
  * 
  * @title
- * 
+ * Summation of primes
  * 
  * @desc
- * 
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
  * 
  * @problem
- * 
+ * Find the sum of all the primes below two million.
  * 
  * @link
- * 
+ * https://projecteuler.net/problem=10
  */
 
-const solution = () => {
-  return 1
+const MAX_PRIME = 2 * 1000 * 1000 // 2 mil
+
+const isPrime = num => {
+  if (num === 2) return true
+  if (num % 2 === 0) return false
+
+  // prime are only odd
+  for (let i = 3; i <= Math.sqrt(num); i += 2) {
+    if (num % i === 0) return false 
+  }
+
+  return true
+}
+
+const solution = max => {
+  let sum = 0
+
+  for (let i = 2; i < max; i++) {
+    sum += isPrime(i) ? i : 0
+  }
+
+  return sum
 }
 
 const now = Date.now()
-const answer = solution()
+const answer = solution(MAX_PRIME)
 
 console.log(`Execution Time: ${Date.now() - now}ms\nAnswer: ${answer}`)
